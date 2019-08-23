209f7ec58d0e6b2e3d7a44059b16cf3e511249d4
zhanghuan <zhanghuan@6f7da4ff-a47d-c84d-9565-2446d01894d9>
2019-08-23 09:30:33 +0000

    2.75-2.77多线程自解锁、原子操作
    git-svn-id: https://desktop-9q6n87v/svn/CppNet100@19 6f7da4ff-a47d-c84d-9565-2446d01894d9

--- IOT/src/CppNet100/HelloThread/main.cpp
+++ IOT/src/CppNet100/HelloThread/main.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 #include <thread>
 #include <mutex>//锁
+#include <atomic>//原子
 #include "CELLTimestamp.hpp"
 using namespace std;
-
+//原子操作（不可分割的操作）计算机处理命令时最小的操作单位
 mutex m;
 const int tCount = 4;
-int sum = 0;
+atomic<int> sum = 0;//可以避免锁的使用
 //说明：主线程和子线程都调用了cout即使有换行，因为没有锁定调用还是有时会并做一行
 //锁：不应该频繁使用；合理锁定区域
 void workFun(int index)
 {	
-	for (int n = 0; n < 20000; n++)
+	for (int n = 0; n < 2000000; n++)
 	{
-		m.lock();//只锁定公共资源，一定要合理
+		//自解锁 出作用域就unlock
+		lock_guard<mutex> lg(m);
+		//m.lock();//只锁定公共资源，一定要合理
 		//临街区域-开始
 		sum++;
 		//临街区域-结束
-		m.unlock();		
+		//m.unlock();		
 	}	
 	//cout << index << "Hello,workFun thread." << n << endl;
 }//抢占式
@@ -36,6 +39,13 @@ int main()
 		t[n].join();//启动线程
 	}
 	cout << tTime.getElapsedTimeInMilliSec()<<",sum=" << sum << endl;
+	sum = 0;
+	tTime.update();
+	for (int n = 0; n < 8000000; n++)
+	{
+		sum++;
+	}
+	cout << tTime.getElapsedTimeInMilliSec() << ",sum=" << sum << endl;
 	cout << "Hello,main thread." << endl;
 	return 0;
 }
